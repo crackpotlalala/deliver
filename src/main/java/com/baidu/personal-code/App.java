4ef4ac0ac2e3d24a828f85d08c14c56721ff1501
gerrit <gerrit@baidu.com>
2019-10-22 17:05:16 +0800

    init

---
+++ src/main/java/com/baidu/personal-code/App.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
+ */
+package com.baidu.personal-code;
+
+/**
+ * Hello world!
+ *
+ * @author java-cmc
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
